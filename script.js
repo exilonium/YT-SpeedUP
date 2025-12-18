88a1435a824664d0f549baa032acb4491728752a
exilonium <exilonium@proton.me>
2025-12-18 22:50:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+
+// ==UserScript==
+// @name        Youtube Speed Control With "a" and "s"
+// @namespace   YoutubeSpeedByExilonium
+// @description Increases the playback speed when the "a" key and decreases when the "s" key is pressed
+// @include     https://www.youtube.com/*
+// @version     1.0.0
+// @grant       none
+// ==/UserScript==
+
+function pressKey(keyCode) {
+  var eventObj = document.createEventObject
+    ? document.createEventObject()
+    : document.createEvent("Events");
+
+  if (eventObj.initEvent) {
+    eventObj.initEvent("keydown", true, true);
+  }
+
+  eventObj.keyCode = keyCode;
+  eventObj.shiftKey = true;
+
+  if (document.dispatchEvent) {
+    document.dispatchEvent(eventObj);
+  } else {
+    document.fireEvent("onkeydown", eventObj);
+  }
+}
+
+document.onkeydown = function (evt) {
+  evt = evt || window.event;
+  var player = document.querySelector("video");
+  if (!player) return;
+
+  // Normalize key
+  var key = evt.key
+    ? evt.key.toLowerCase()
+    : String.fromCharCode(evt.keyCode).toLowerCase();
+
+  switch (key) {
+    case "r":
+      var player = document.querySelector(".html5-main-video");
+      if (player) {
+        // Calculate how many times to press Shift + . or Shift + ,
+        var step = 0.25; // YouTube's normal step size
+        var target = 1.0; // Normal speed
+        var diff = player.playbackRate - target;
+
+        if (diff > 0) {
+          var presses = Math.round(diff / step);
+          for (var i = 0; i < presses; i++) pressKey(188); // Shift + , to slow down
+        } else if (diff < 0) {
+          var presses = Math.round(-diff / step);
+          for (var i = 0; i < presses; i++) pressKey(190); // Shift + . to speed up
+        }
+      }
+      break;
+
+    case "s":
+      pressKey(190); // speed up
+      break;
+
+    case "a":
+      pressKey(188); // slow down
+      break;
+  }
+};
